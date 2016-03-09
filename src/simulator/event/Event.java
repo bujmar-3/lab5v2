afa3fbc5b9d29906bc52cdc4c98352b2b0e38c63
Arvidp96 <arvper-5@student.ltu.se>
2016-03-09 20:41:06 +0100

    Raderade compareTo i SortedSequence

--- src/simulator/event/Event.java
+++ src/simulator/event/Event.java
@@ -13,13 +13,11 @@ import java.lang.Comparable;
 	public abstract class Event implements Comparable<Event> { 
 		protected double time; 
  
- 
 /** 
  * Constructs an Event 
  */ 
  	public Event() { 
  	} 
- 
  
  	/** 
  	 * Constructs an Event with a given time that it should occur 
@@ -46,13 +44,4 @@ import java.lang.Comparable;
  	 */ 
  	public abstract void updateState(SimState state, EventQueue eventQueue); 
  
- 
- 	public int compareTo(Event event) { 
- 		if (this.time < event.time) { 
- 			return -1; 
- 		} else if (this.time > event.time) { 
- 			return 1; 
- 		} 
- 		return 0; 
- 	} 
  } 
