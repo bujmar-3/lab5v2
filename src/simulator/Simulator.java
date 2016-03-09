9af38972335e2bf5c82883d1be2b2c2827140862
bujmar-3 <marz@hotmail.se>
2016-03-09 21:22:58 +0100

    javadoc för simulator

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -2,17 +2,29 @@ package simulator;
 
 import simulator.state.SimState;
 import simulator.event.*;
-
+/**
+ * A discrete event-driven simulator that simulates a state.
+ * */
 public class Simulator {
 	private SimState state;
 	private EventQueue eventQueue;
 	
+	/**
+	 * Creates the simulator.
+	 * @param s The SimState used in the simulation.
+	 * @param e The EventQueue used for the simulation.
+	 * */
 	public Simulator(SimState s, EventQueue e){
 		state = s;
 		eventQueue = e;
 	}
+	/**
+	 * Gets the first event in eventQueue and executes the code in method updateState
+	 *  in each event which changes the state of the simulator. 
+	 *  If the event calls stop method in the state or the event queue is empty simulation stops.
+	 * */
 	public void execute(){
-		while (state.state()){
+		while (state.state() && eventQueue.size()!=0){
 			Event nextEvent = eventQueue.get();
 			nextEvent.updateState(state, eventQueue);
 		}
